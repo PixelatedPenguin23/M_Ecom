efd810c86e09fe460eaa59632103f5598ea4323b
PixelatedPenguin23 <slowlanewave@gmail.com>
2024-08-21 13:59:23 +0330

    k

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@components/Nav";
+import { Provider } from "react-redux";
+import { store } from "@redux/store";
 
 const inter = Inter({ subsets: ["latin"],weight:'700' });
 
@@ -11,13 +13,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    
     <html lang="en">
+      <Provider store={store}>
       <body className={`${inter.className} antialiased`}>
         <Nav/>
         <main className="px-40 mt-7">
         {children}
         </main>
       </body>
+      </Provider>
     </html>
   );
 }
---
+++ redux/reducer.js
@@ -0,0 +1,15 @@
+const init={
+  count:0
+}
+
+export const Reducer=(state=init,action)=>{
+  switch(action.type){
+    case "INC":return{
+      ...state,count:state.count+1
+    };
+    case "DEC":return{
+      ...state,count:state.count-1
+    };
+    default:return{state}
+  }
+}
